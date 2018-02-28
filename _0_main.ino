4194fa7510e9b681a0ea036083c98bef8bec8732
Muhammad Ansyar Rafi Putra <33691015+mansyar@users.noreply.github.com>
2018-02-28 09:05:07 +0100

    Add files via upload

---
+++ _0_main.ino
@@ -0,0 +1,31 @@
+#include <Arduino_FreeRTOS.h> //Free-RTOS lib
+#include <semphr.h>
+#include <task.h>
+#include <SPI.h>
+
+#include <Melon_MS5607.h> //pressure sensor lib
+#include <HDC2010.h> //humidity sensor lib
+#include <Temperature_LM75_Derived.h> //temperature sensor lib
+
+
+/*Global variables*/
+SemaphoreHandle_t sem = NULL;
+static int state;
+static int samplingRate;
+static float tempReading [4]; //array size might change according to the fix quantity
+static float humReading [1];
+static float pressReading [2];
+
+
+/*init*/
+void setup()
+{
+    sem = xSemaphoreCreateBinary();
+    init();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
