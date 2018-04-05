cc8896b44ad19188682183818c7cbd47037bf780
Muhammad Ansyar Rafi Putra <33691015+mansyar@users.noreply.github.com>
2018-04-05 15:22:18 +0200

    Revert "Merge pull request #10 from mansyar/Gustav"
    This reverts commit 156b0a9beb3b9889f096b996d1e83627d4efba2f, reversing
    changes made to f3efbe6b8e87a9b58e6d5c977dab89a57e517e24.

--- onboard-software/_0_main/_0_main.ino
+++ onboard-software/_0_main/_0_main.ino
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <Arduino_FreeRTOS.h> //Free-RTOS lib
 #include <semphr.h>
 #include <task.h>
@@ -33,4 +34,39 @@ void setup()
 void loop() {
   // put your main code here, to run repeatedly:
 
+=======
+#include <Arduino_FreeRTOS.h> //Free-RTOS lib
+#include <semphr.h>
+#include <task.h>
+#include <SPI.h>
+#include <stdbool.h>
+
+#include <Melon_MS5607.h> //pressure sensor lib
+#include <HDC2010.h> //humidity sensor lib
+#include <Temperature_LM75_Derived.h> //temperature sensor lib
+
+
+/*Global variables*/
+SemaphoreHandle_t sem = NULL;
+static int state;
+static int samplingRate;
+static float tempReading [4]; //array size might change according to the fix quantity
+static float humReading [1];
+static float pressReading [2]; //array size might change according to the fix quantity
+static int htrParameter[4];
+static float ascParameter[20];
+
+
+/*init*/
+void setup()
+{
+    sem = xSemaphoreCreateBinary();
+    init();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+>>>>>>> parent of 156b0a9... Merge pull request #10 from mansyar/Gustav
 }
