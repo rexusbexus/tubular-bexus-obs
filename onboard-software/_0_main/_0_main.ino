cec15cf80cc62b471abc0b0d85961e3c43ed028b
Gustav Dyrssen <gustav.d.man@gmail.com>
2018-04-05 14:26:09 +0200

    Add files via upload

---
+++ onboard-software/_0_main/_0_main.ino
@@ -0,0 +1,72 @@
+#include <Arduino_FreeRTOS.h> //Free-RTOS lib
+#include <semphr.h>
+#include <task.h>
+#include <SPI.h>
+#include <stdbool.h>
+
+#include <Ethernet2.h>
+#include <EthernetUdp2.h>
+
+#include <Melon_MS5607.h> //pressure sensor lib
+#include <HDC2010.h> //humidity sensor lib
+#include <Temperature_LM75_Derived.h> //temperature sensor lib
+
+
+/*Global variables*/
+#define pumpPin 20
+#define valve1 21
+#define valve2 22
+#define valve3 23
+#define valve4 24
+#define valve5 25
+#define valve6 26
+#define valve7 27
+#define valve8 28
+#define valve9 29
+#define valve10 30
+#define flushValve 31
+#define CACvalve 32
+#define htr1_pin 33
+#define htr2_pin 34
+
+#define nrTempSensors   6
+#define nrPressSensors  6
+#define nrHumidSensors  2
+#define nrAirFSensors   1
+int loremSensor;
+
+
+
+SemaphoreHandle_t sem = NULL;
+static int state;
+static int samplingRate;
+static float tempReading [nrTempSensors]; //array size might change according to the fix quantity
+static float humReading [nrHumidSensors];
+static float pressReading [nrPressSensors]; //array size might change according to the fix quantity
+static float humidReading[nrAirFSensors]
+static int htrParameter[4];
+static float ascParameter[20];
+double status=0;
+
+byte mac[] = { 0x2C, 0xF7, 0xF1, 0x08, 0x0F, 0x57 };
+IPAddress ip(1, 1, 1, 1); //Local IPv4 adress
+//IPAddress myDns(192,168,1, 1);
+//IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 0, 0, 0);
+IPAddress remote(1, 1, 1, 2);
+unsigned int localPort = 8888;
+EthernetUDP Udp;
+
+
+/*init*/
+void setup()
+{
+    sem = xSemaphoreCreateBinary();
+    init();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ onboard-software/_0_main/_4_heater.ino
@@ -0,0 +1,152 @@
+/*
+ * Name: Heater object
+ * Purpose: To read the temperature sensor for sensorbox
+ * and valve sensor and accordingly turn on/off heater.
+ * Project: Tubular-Bexus.
+ * Authors: Tubular-Bexus software group.
+*/
+
+
+
+void initHeater() {
+  
+  initReadingData();
+  //int htrParameter[4];
+}
+
+void initReadingData() {
+    xTaskCreate(
+    readingData
+    ,  (const portCHAR *) "readingData";   // Name
+    ,  128  // This stack size 
+    ,  NULL
+    ,  2  // Priority
+    ,  NULL );
+
+}
+
+/* readingData child object
+ * readingData reads the temperature data at valve center and electronics box.
+ * Turn on/off the two heaters accoring to parameters.
+ */
+void readingData()
+{
+  float tempAtHtr [2];
+  
+  TickType_t xLastWakeTime;
+  xLastWakeTime = xTaskGetTickCount ();
+  
+  while(1)
+  {
+    //Reads the current mode
+    static int currMode = getMode();
+
+    //Reads the temperature at the two sensors
+    float tempAtHts[0]  = readData(0);
+    float tempAtHts[1]  = readData(0);//Must make sure to get the correct tempSensors and add pointer.
+    bool  htr1_flag;
+    bool  htr2_flag;
+    int *htrParam;
+
+    //Compares with parameters
+    if (tempAtHts[0]=<0 || tempAtHts[0]=>16777215 || tempAtHts[1]=<0 || tempAtHts[1]=>16777215)
+    {
+      /*
+       * Check if within correct value of parameters
+       * Also works for errors in tempAtHts[]
+       */
+    }    
+    else if (*(htrParam+0)<tempAtHts[0])
+    {
+        htr1_flag = 1;
+    }
+    else if (*(htrParam+1)>tempAtHts[0])
+    {
+        htr1_flag = 0;
+    }
+    else if (*(htrParam+2)<tempAtHts[1])
+    {
+        htr2_flag = 1;
+    }
+    else if (*(htrParam+3)>tempAtHts[1])
+    {
+        htr2_flag = 0;
+    }
+    
+    if htr1_flag ^ htr2_flag  // if 0 1 or 1 0
+    {
+      heaterControl(htr1_flag,htr1_flag);
+    }
+    else if htr1_flag & htr2_flag // if 1 1
+    {
+      /*
+       * If both heaters require to be turned on it will choose the one most
+       * largest difference between their paramters and actual temperature.
+       */
+      if (*(htrParam+0) - tempAtHts[0])>( *(htrParam+2) - tempAtHts[1])
+      {
+        heaterControl(1,0);
+      }
+      else if (*(htrParam+0) - tempAtHts[0])<( *(htrParam+2) - tempAtHts[1])
+      {
+        heaterControl(0,1);
+      }
+      else // if 0 0
+      {
+        heaterControl(htr1_flag,htr1_flag);
+        /*
+        * The heaters does not need to operate
+        */
+      }
+      
+    } 
+    vTaskDelayUntil(&xLastWakeTime, (5000 / portTICK_PERIOD_MS) ); 
+  }
+    
+}
+/*
+ * END of readingData child object
+ */
+
+
+/* The parameter child object.
+ *  int * heaterParameter returns the pointer
+ *  to htrParam.
+ */
+int * readHeaterParameter()
+{
+  static int htrParam[4];
+  xSemaphoreTake(sem, portMAX_DELAY);
+  htrParam = htrParameter;
+  xSemaphoreGive(sem);    
+  return htrParam;
+}
+
+/* setHeaterParameter has an array of four(4) 
+ *  elemnets and will replace the htrParameter array
+ *  with new values
+ */
+void setHeaterParameter(int newParameter[4])
+{
+  xSemaphoreTake(sem, portMAX_DELAY);
+  htrParameter=newParameter;
+  xSemaphoreGive(sem);
+}
+/*
+ * END of parameter child object
+ */
+
+/*
+ * The heater control child object
+ * 
+ */
+void heaterControl(htrOne,htrTwo)
+{
+  xSemaphoreTake(sem, portMAX_DELAY);
+  digitalWrite(htr1_pin,htrOne)
+  digitalWrite(htr2_pin,htrTwo)
+  xSemaphoreGive(sem);    
+}
+/*
+ * End of the heaterControl child object
+ */
