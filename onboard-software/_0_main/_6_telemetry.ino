fbcfeeb6d79257d1c359fc6d8357a7162c71fcd9
Gustav Dyrssen <gustav.d.man@gmail.com>
2018-04-10 14:09:59 +0200

    Fixed heater and telemetry objects

--- onboard-software/_0_main/_6_telemetry.ino
+++ onboard-software/_0_main/_6_telemetry.ino
@@ -1,58 +1,62 @@
 /* Telemtery object
- * Authors: BEXUS-TUBULAR software team
- * 
- */
+   Authors: BEXUS-TUBULAR software team
 
- 
+*/
+
+
 
 void transmit() {
-  tempData = readData(0);
-  humidData = readData(1);
-  PressData = readData(2);
-  airFData = readData(3);
-  
+  std::vector<float>  tempData = readData(0);
+  std::vector<float>  humidData = readData(1);
+  std::vector<float>  pressData = readData(2);
+  std::vector<float>  airFData = readData(3);
+
   Udp.beginPacket(remote, localPort);
-  tempSensor=nrPressSensors;
-  Udp.write("tub,ps",tempSensor,",");
-  for (int i=0;i<tempSensor-1;i++) //Loop number of time there are press sensors
+  Udp.write("tub,ps");
+  Udp.write(nrPressSensors);
+  for (int i = 0; i < nrPressSensors; i++) //Loop number of time there are press sensors
   {
-    Udp.write(*(pressData+i));
+    Udp.write(pressData[i]);
+   // Udp.write(",");
+  }
+
+
+  Udp.write("ts,");
+  Udp.write(nrTempSensors);
+  for (int i = 0; i < nrTempSensors; i++) //Loop number of time there are temp sensors
+  {
+    
+    Udp.write(tempData[i]);
     Udp.write(",");
   }
 
-  loremSensor=nrTempSensors;
-  Udp.write("ts,",loremSensor,",");
-  for (int i=0;i<nrloremSensors-1;i++) //Loop number of time there are temp sensors
+  Udp.write("ts,");
+  Udp.write(nrAirFSensors);
+  for (int i = 0; i < nrAirFSensors; i++) //Loop number of time there are air flow sensors
   {
-    Udp.write(*(tempData+i));
+    Udp.write(airFData[i]);
     Udp.write(",");
   }
 
-  Udp.write("ts,",nrAirFSensors,",");
-  for (int i=0;i<airFSensors-1;i++) //Loop number of time there are air flow sensors
+  Udp.write("ts,");
+  Udp.write(nrHumidSensors);
+  for (int i = 0; i < nrHumidSensors; i++) //Loop number of time there are humidity sensors
   {
-    Udp.write(*(airFData+i));
+    Udp.write((humidData[i]));
     Udp.write(",");
   }
 
-  Udp.write("ts,",nrHumidSensors,",");
-  for (int i=0;i<humidSensors-1;i++) //Loop number of time there are humidity sensors
-  {
-    Udp.write(*(humidData+i));
-    Udp.write(",");
-  }
 
-  
   Udp.write("st,");
-  for (int i=0;i<= (htr2_pin - pumpPin) ;i++) //Loop number of time there are humidity sensors
+  for (int i = 0; i <= (htr2_pin - pumpPin) ; i++) //Loop number of time there are humidity sensors
   {
-    status = status + digitalRead(pumpPin+i)
-    status << status;
+    status = status + status + digitalRead(pumpPin + i);
+             //status << status;
   }
   Udp.write(status);
   Udp.write(getMode());
   Udp.write("\n");
   Udp.endPacket();
-  
+
 
 }
