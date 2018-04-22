082214037c0a369cd7f69617d048b8df216786e3
Gustav Dyrssen <gustav.d.man@gmail.com>
2018-04-22 21:10:27 +0200

    Reworked the library files for the MS5607 sensor

--- libraries/MS5607.h
+++ libraries/MS5607.h
@@ -8,13 +8,16 @@ class MS5607
 {
 //reset_sequence()
     protected:
+        int _PIN;
         
     Public:
+        MS5607(int PIN);
         void reset_sequence();
         void convertionD1(int i);
         void convertionD2(int i);
         uint16_t PROMread(uint8_t i);
         uint132_t readADC();
+        uint32_t readADC_calc(uint16_t PROMbyte[8], int i)
     
     
 };
