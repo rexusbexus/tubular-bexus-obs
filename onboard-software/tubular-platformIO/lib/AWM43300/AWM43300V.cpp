a1914819937e9f63711de7af9e2440c0b996a1b5
Muhammad Ansyar Rafi Putra <33691015+mansyar@users.noreply.github.com>
2018-09-04 14:03:58 +0200

    added library to lib

---
+++ onboard-software/tubular-platformIO/lib/AWM43300/AWM43300V.cpp
@@ -0,0 +1,15 @@
+#include <AWM43300V.h>
+
+AWM43300V::AWM43300V(const byte pin)
+{
+	// Arduino analog pin the sensor is connected to
+	_pin = pin;
+}
+
+float AWM43300V::getAF()
+{
+	uint16_t sensVal = analogRead(_pin);
+	float AF = 0;
+	AF = 12.184*(sensVal^4) - 108.71*(sensVal^3) + 344.34*(sensVal^2) - 359.86*sensVal + 100;
+	return AF;
+}
